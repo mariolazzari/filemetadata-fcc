9cd56122edce23a8b78cf41d65b83d227ec9baec
Mario Lazzari <mario.lazzari@gmail.com>
2023-09-20 20:50:35 +0200

    feat: file api

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-var express = require("express");
-var cors = require("cors");
+const express = require("express");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+
 require("dotenv").config();
+const port = +process.env.PORT || 3000;
 
-var app = express();
-
+const app = express();
 app.use(cors());
+app.use(
+  fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 },
+  })
+);
 app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get("/", function (req, res) {
+app.get("/", (_req, res) => {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.post("/api/fileanalyse", (req, res) => {
+  const { upfile } = req.files;
+  const { name, mimetype: type, size } = upfile;
+
+  res.status(200).json({ name, type, size });
+});
+
+app.listen(port, () => {
   console.log("Your app is listening on port " + port);
 });
